95fe78402c6c175e52df0242937065bb0ec52067
Ali Moghimi <mghmiali@gmail.com>
2025-01-18 13:07:00 +0000

    feat: main module

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+from client.engine import Engine
+from config import logger
+from database.helper import init_db_instance, load_tickers
+from transformer import Agent
+
+
 def main():
-    pass
+    logger.info("Loading tickers...")
+    tickers = load_tickers()[:100]
+    logger.info(f"{len(tickers)} Tickers loaded from database.")
+    logger.info("Initializing Engine...")
+    engine = Engine(tickers)
+    logger.info("Engine initialized successfully.")
+    logger.info("Running Engine...")
+    engine.run()
+    logger.info("Engine run completed successfully.")
+
+    logger.info("Transforming data...")
+    transformer = Agent(engine.data)
+    tables = transformer.transform()
+
+    logger.info("Saving data to database...")
+    conn = init_db_instance()
+    for t, dataframe in tables.items():
+        if not dataframe.empty:
+            logger.info(f"\n{dataframe}")
+            conn.insert_table(dataframe, t)
+
+    logger.info("Process completed successfully")
 
 
 if __name__ == "__main__":
